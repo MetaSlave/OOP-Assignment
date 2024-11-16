1282a29022ad3797cae2c1ff27d88be0c0a8d158
MetaSlave <chongtinghao@gmail.com>
2024-11-16 23:41:41 +0800

    changes

--- DoctorManager.java
+++ DoctorManager.java
@@ -8,24 +8,11 @@ import java.util.Objects;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class DoctorManager implements IMenu, IViewMedicalRecord, IViewScheduledAppointments, ICheckMedicineExists {
+public class DoctorManager implements IViewMedicalRecord, IViewScheduledAppointments, ICheckMedicineExists {
     // Use database singleton
     private final HMSDatabase db = HMSDatabase.getInstance();
     // Use scanner singleton
     private final Scanner scanner = HMSInput.getInstance().getScanner();
-    
-    @Override
-    public void display() {
-        System.out.println("----DOCTOR MENU----");
-        System.out.println("1. View Patient Medical Records");
-        System.out.println("2. Update Patient Medical Records");
-        System.out.println("3. View Personal Schedule");
-        System.out.println("4. Set Availability for Appointments");
-        System.out.println("5. Accept or Decline Appointment Requests");
-        System.out.println("6. View Upcoming Appointments");
-        System.out.println("7. Record Appointment Outcome");
-        System.out.println("8. Logout");
-    }
 
     public void viewMedicalRecord(User d) {
         // Get all patients associated with doctor
